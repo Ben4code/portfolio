6d069a040053b279f63e04f118ae83dd9d9fee13
nnaemeka obioha <nnaemeka.obioha@gmail.com>
2019-04-29 21:39:56 +0100

    Modified favicon url 2

--- client/src/components/Layouts/Navbar.js
+++ client/src/components/Layouts/Navbar.js
@@ -42,7 +42,8 @@ class Navbar extends Component {
                 <li className="menu-item"><a href="/" onClick={this.onLogOut.bind(this)}>Logout</a></li>
                 <li className="menu-item">
                     <a href="/">
-                        <img src={ isAuth ? window.location.origin + '/' + user.avatar.substr(14) : ''} width="30" alt="User avatar"/>
+                        {/* <img src={ isAuth ? window.location.origin + '/' + user.avatar.substr(14) : ''} width="30" alt="User avatar"/> */}
+                        <img src={ isAuth ? process.env.PUBLIC_URL + '/' + user.avatar.substr(14) : ''} width="30" alt="User avatar"/>
                     </a>
                 </li>
             </ul>
